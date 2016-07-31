07526f20b65a21bd6e79f32a1e5656e53620f448
mjoyner <mjoyner@vbservices.net>
2016-07-31 16:49:37 -0400

    jitpack.io + update gradle wrapper

--- CherokeeAffixSplitter/src/main/java/com/cherokeelessons/affixsplitter/Main.java
+++ CherokeeAffixSplitter/src/main/java/com/cherokeelessons/affixsplitter/Main.java
@@ -1,13 +1,14 @@
 package com.cherokeelessons.affixsplitter;
 
 import java.awt.EventQueue;
+import java.io.IOException;
 
-import com.newsrx.gui.MainWindow;
-import com.newsrx.gui.MainWindow.Config;
+import com.cherokeelessons.gui.MainWindow;
+import com.cherokeelessons.gui.MainWindow.Config;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		MainWindow.Config config = new Config() {
 			@Override
 			public String getApptitle() {
