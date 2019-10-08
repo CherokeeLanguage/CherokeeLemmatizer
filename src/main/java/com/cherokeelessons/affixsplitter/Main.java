8f354455301228c12a3646fbbfd0ad376d5c3743
mjoyner <mjoyner@vbservices.net>
2019-10-07 21:10:46 -0400

    Fix up dependencies.

--- src/main/java/com/cherokeelessons/affixsplitter/Main.java
+++ src/main/java/com/cherokeelessons/affixsplitter/Main.java
@@ -1,26 +1,11 @@
 package com.cherokeelessons.affixsplitter;
 
-import java.awt.EventQueue;
 import java.io.IOException;
-
-import com.cherokeelessons.gui.MainWindow;
-import com.cherokeelessons.gui.MainWindow.Config;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		MainWindow.Config config = new Config() {
-			@Override
-			public String getApptitle() {
-				return "Cherokee Affix Splitter";
-			}
-			
-			@Override
-			public Thread getApp(String... args) throws Exception {
-				return new AffixSplitter(args);
-			}
-		};
-		EventQueue.invokeLater(new MainWindow(config, args));
+		new AffixSplitter(args).run();
 	}
 
 }
