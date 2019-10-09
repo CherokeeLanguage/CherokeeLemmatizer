836908b72a2f76c7e0ae20738cdd714ed0b24f25
mjoyner <mjoyner@vbservices.net>
2019-10-09 15:53:53 -0400

    more adjustment to new factored form model

--- src/main/java/com/cherokeelessons/lemmatizer/FactoredForm.java
+++ src/main/java/com/cherokeelessons/lemmatizer/FactoredForm.java
@@ -5,28 +5,62 @@ public class FactoredForm {
 	private String pos;
 	private String lemma;
 	private String stem;
-	public String getSurfaceForm() {
-		return surfaceForm;
+	private String prefixes;
+	private String suffixes;
+
+	public void addPrefix(String prefix) {
+		prefixes = getPrefixes() + prefix + "-";
 	}
+
+	public void addSuffix(String suffix) {
+		suffixes = "-" + suffix + getSuffixes();
+	}
+
+	public String getSurfaceForm() {
+		return surfaceForm == null ? "" : surfaceForm;
+	}
+
 	public void setSurfaceForm(String surfaceForm) {
 		this.surfaceForm = surfaceForm;
 	}
+
 	public String getPos() {
-		return pos;
+		return pos == null ? "" : pos;
 	}
+
 	public void setPos(String pos) {
 		this.pos = pos;
 	}
+
 	public String getLemma() {
-		return lemma;
+		return lemma == null ? "" : lemma;
 	}
+
 	public void setLemma(String lemma) {
 		this.lemma = lemma;
 	}
+
 	public String getStem() {
-		return stem;
+		return stem == null ? "" : stem;
 	}
+
 	public void setStem(String stem) {
 		this.stem = stem;
+	}
+
+	public String getPrefixes() {
+		return prefixes == null ? "" : prefixes;
+	}
+
+	public void setPrefixes(String prefixes) {
+		this.prefixes = prefixes;
+	}
+
+	public String getSuffixes() {
+		return suffixes == null ? "" : suffixes;
+	}
+
+	public void setSuffixes(String suffixes) {
+		this.suffixes = suffixes;
 	}
 }
